3d5c7926744e6bb326a14d48683536af30a6805c
Igor Gunin <gunin-igor75@yandex.ru>
2022-07-13 19:47:15 +0600

    final commit

--- src/Main.java
+++ src/Main.java
@@ -58,29 +58,31 @@ public class Main {
 
     public static void task5() {
         int monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осень");
-                break;
-            default:
-                break;
+        if (monthNumber > 0 && monthNumber <= 12) {
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
